f0ef04f6a9dacbfa5ff5fa2b4d15ca29a82e7d03
kvan7 <kvan.valve@gmail.com>
2026-07-17 19:11:02 -0500

    dump

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <MainWindow />
+  </div>
 </template>
+<script setup lang="ts">
+import MainWindow from './web/MainWindow.vue'
+</script>
 
-<style scoped>
-@import 'tailwindcss';
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
---
+++ src/web/MainWindow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex h-screen w-screen flex-col bg-slate-800"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
