205c04fd06101e59371486dfc2351b040ff099cd
kvan7 <kvan.valve@gmail.com>
2026-07-18 00:43:28 -0500

    banana

--- src/web/MyHeader.vue
+++ src/web/MyHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <header class="bg-background-darker flex h-6 w-full justify-between px-4">
-    <div>This is just some demo to help me debug which recipes are actually used</div>
+    <div>
+      This is just some demo to help me debug which recipes are actually used, double click row in
+      top to tables to select
+    </div>
     <a href="https://github.com/Kvan7/test-remnants">Github</a>
   </header>
 </template>
